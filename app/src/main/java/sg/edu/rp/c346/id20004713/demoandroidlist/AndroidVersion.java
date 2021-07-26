313cc33bb0fa67234933e9553902021907d01ad4
Jim Chiew <20004713@myrp.edu.sg>
2021-07-26 12:26:56 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20004713/demoandroidlist/AndroidVersion.java
@@ -0,0 +1,42 @@
+package sg.edu.rp.c346.id20004713.demoandroidlist;
+
+import androidx.annotation.NonNull;
+
+public class AndroidVersion {
+    private String name;
+    private  String version;
+
+    public AndroidVersion(String name, String version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    /*
+    @NonNull
+    @Override
+    public String toString() {
+        return "name= '" + name + "', version='" + version + "'";
+    } */
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name + ": " + version;
+    }
+}
